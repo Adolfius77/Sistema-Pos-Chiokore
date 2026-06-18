d6316a12359bf08aaa1b666ef1c3ac8f6feb6eca
Adolfo Ortega <jose.ortega252882@potros.itson.edu.mx>
2026-06-17 19:57:23 -0700

    pruebas con keycloak

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,41 @@ import './styles/app.scss'
 import Navbar from "./Componentes/Navbar.jsx";
 import SideBar from "./Componentes/sideBar.jsx";
 
+import Keycloak from "keycloak-js";
+
 import { Routes, Route } from "react-router-dom";
 
 import Categorias from "./pages/categorias.jsx";
 import Carrito from "./pages/carrito.jsx";
+import {useEffect, useState} from "react";
 
+const KeyCloakOptions = {
+    url: "http://localhost:9090",
+    realm: "sistema-pos-chiokore",
+    clientId: ""
+}
 function App() {
+    const [keycloak, setKeycloak] = useState(null)
+    useEffect(() => {
+        const initKeycloak = async () =>{
+            const keycloakInstance = new Keycloak(KeyCloakOptions);
+            try{
+                await keycloakInstance.init({ onLoad: "login-required" });
+                setKeycloak(keycloakInstance);
+                if (keycloakInstance.authenticated) {
+                    console.log(keycloakInstance);
+                }
+            }catch (error){
+                console.error("Error initializing Keycloak:", error);
+            }
+        }
+        initKeycloak();
+    }, []);
+    const handleLogout = () => {
+        if(keycloak){
+            keycloak.logout();
+        }
+    }
     return (
         <>
             <Navbar />
